80ccc787d8b7a9b682d570411fa324b4b5ddcd20
Chris McCutcheon <cmccutcheon2015@gmail.com>
2017-11-16 15:55:07 -0800

    finish user signup

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,6 +7,6 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get signup_path
     assert_response :success
-    assert_select "title", "Signup | #{@base_title}"
+    assert_select "title", "Sign up | #{@base_title}"
   end
 end
